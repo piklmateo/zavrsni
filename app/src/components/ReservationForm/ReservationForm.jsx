8ea2a57383a7b823924c06250932b3a8635a86af
Mateo <piklmateo07@gmail.com>
2024-04-22 13:34:04 +0200

    Implemented 3 step reservation form.

--- app/src/components/ReservationForm/ReservationForm.jsx
+++ app/src/components/ReservationForm/ReservationForm.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-
+import LeftArrow from "../../assets/images/left-arrow.svg";
+import RightArrow from "../../assets/images/right-arrow.svg";
 import { jwtDecode } from "jwt-decode";
 import "./ReservationForm.css";
 
 const ReservationForm = () => {
+  const [step, setStep] = useState(1);
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -61,83 +63,110 @@ const ReservationForm = () => {
     }
   }, []);
 
-  useEffect(() => {});
-
   return (
     <div className="main__layout__container">
       <div className="reservation__form__wrapper">
         <h1>Reservation</h1>
         <form className="form">
-          <div className="input__name">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={formData.name || ""}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="input__email">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={formData.email || ""}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="input__phone">
-            <label htmlFor="phone">Phone(optional)</label>
-            <input
-              type="tel"
-              name="phone"
-              id="phone"
-              value={formData.phone || ""}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="input__date">
-            <label htmlFor="date">Date</label>
-            <input type="date" name="date" id="date" />
-          </div>
-          <div className="select__time">
-            <label htmlFor="time">Time</label>
-            <select name="time" id="time">
-              <option value="11:00">11:00 AM</option>
-              <option value="12:00">12:00 AM</option>
-              <option value="13:00">13:00 PM</option>
-              <option value="14:00">14:00 PM</option>
-              <option value="15:00">15:00 PM</option>
-              <option value="16:00">16:00 PM</option>
-              <option value="17:00">17:00 PM</option>
-              <option value="18:00">18:00 PM</option>
-              <option value="19:00">19:00 PM</option>
-              <option value="20:00">20:00 PM</option>
-              <option value="21:00">21:00 PM</option>
-              <option value="22:00">22:00 PM</option>
-            </select>
-          </div>
-          <div className="select__num-of-people">
-            <label htmlFor="people">How many people ?</label>
-            <select name="people" id="people">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-            </select>
-          </div>
-          <div>
-            <button className="btn btn__reserve">Reserve</button>
-          </div>
+          {step === 1 && (
+            <div className="input__date">
+              <label htmlFor="date">Date</label>
+              <input
+                type="date"
+                name="date"
+                id="date"
+                value={formData.date}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          )}
+          {step === 2 && (
+            <div className="select__time">
+              <label htmlFor="time">Time</label>
+              <select
+                name="time"
+                id="time"
+                value={formData.time}
+                onChange={handleChange}
+              >
+                {/* Options for time */}
+              </select>
+            </div>
+          )}
+          {step === 3 && (
+            <>
+              <div className="input__name">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="input__email">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="input__phone">
+                <label htmlFor="phone">Phone(optional)</label>
+                <input
+                  type="tel"
+                  name="phone"
+                  id="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <button className="btn btn__reserve">Reserve</button>
+              </div>
+            </>
+          )}
         </form>
+        <div className="navigation__buttons">
+          {step !== 1 && (
+            <div>
+              <button
+                className="navigation__btn"
+                onClick={() => setStep(step - 1)}
+              >
+                <img alt="arrow-back" src={LeftArrow} width={40} height={40} />
+              </button>
+            </div>
+          )}
+          {step !== 3 && (
+            <div>
+              <button
+                className="navigation__btn"
+                onClick={() => setStep(step + 1)}
+              >
+                <img alt="arrow-next" src={RightArrow} width={40} height={40} />
+              </button>
+            </div>
+          )}
+        </div>
+        <div className="step__indicator">
+          <div
+            className={`step__line ${step >= 1 ? "step__active" : ""}`}
+          ></div>
+          <div
+            className={`step__line ${step >= 2 ? "step__active" : ""}`}
+          ></div>
+          <div
+            className={`step__line ${step >= 3 ? "step__active" : ""}`}
+          ></div>
+        </div>
       </div>
     </div>
   );
