630936f37ac5889ec3d9be0e848bbfa9d4b24167
Mateo <piklmateo07@gmail.com>
2024-05-17 15:51:46 +0200

    Implemented mobile nav.

--- app/src/components/NavBar/NavBar.tsx
+++ app/src/components/NavBar/NavBar.tsx
@@ -15,6 +15,7 @@ interface DecodedToken extends JwtPayload {
 const NavBar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [allowedRoutes, setAllowedRoutes] = useState<string[]>([]);
+  const [isMobileNav, setIsMobileNav] = useState<boolean>(false);
 
   const roleRoutes: { [key: number]: string[] } = {
     1: ["/menu", "/profile", "/reservations", "/add-dish", "/add-drink", "/order", "/order-list", "/statistics"], // ADMIN
@@ -39,36 +40,44 @@ const NavBar = () => {
     window.location.href = "/login";
   };
 
+  const handleMobileNav = () => {
+    setIsMobileNav(!isMobileNav);
+  };
+
   return (
     <div className="nav__header">
       <div className="main__layout__container">
         <div className="container">
           <div className="logo__container">
             <img className="logo__navigation" src={logo} alt="logo" />
+            <div className="burger__menu" onClick={handleMobileNav}>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+            </div>
           </div>
-          <nav className="nav__menu">
+          <nav className={`nav__menu ${isMobileNav ? "nav__menu--open" : ""}`}>
             <ul className="nav__list">
               <li className="nav__list__item">
-                <Link className="nav__link" to="/">
+                <Link className="nav__link" to="/" onClick={handleMobileNav}>
                   Home
                 </Link>
               </li>
               <li className="nav__list__item">
-                <Link className="nav__link" to="/menu">
+                <Link className="nav__link" to="/menu" onClick={handleMobileNav}>
                   Menu
                 </Link>
               </li>
               <li className="nav__list__item">
-                <Link className="nav__link" to="/reservation">
+                <Link className="nav__link" to="/reservation" onClick={handleMobileNav}>
                   Reservation
                 </Link>
               </li>
               <li className="nav__list__item">
-                <Link className="nav__link" to="/special-occasions">
+                <Link className="nav__link" to="/special-occasions" onClick={handleMobileNav}>
                   Special occasions
                 </Link>
               </li>
-
               {isLoggedIn &&
                 allowedRoutes.map((route, index) => {
                   if (route === "/reservations") {
@@ -124,24 +133,23 @@ const NavBar = () => {
                 })}
             </ul>
           </nav>
-
-          <div className="contact__container">
-            {!isLoggedIn && (
-              <>
-                <Link to="/login">
-                  <button className="btn btn__login">Login</button>
-                </Link>
-                <Link to="/registration">
-                  <button className="btn btn__register">Register</button>
-                </Link>
-              </>
-            )}
-
-            {isLoggedIn && (
-              <button className="btn btn__logout" onClick={handleLogout}>
-                Logout
-              </button>
-            )}
+          <div className={`nav__menu ${isMobileNav ? "nav__menu--open" : ""}`}>
+            <div className="contact__container">
+              {!isLoggedIn ? (
+                <>
+                  <Link to="/login" onClick={handleMobileNav}>
+                    <button className="btn btn__login">Login</button>
+                  </Link>
+                  <Link to="/registration" onClick={handleMobileNav}>
+                    <button className="btn btn__register">Register</button>
+                  </Link>
+                </>
+              ) : (
+                <button className="btn btn__logout" onClick={handleLogout}>
+                  Logout
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
