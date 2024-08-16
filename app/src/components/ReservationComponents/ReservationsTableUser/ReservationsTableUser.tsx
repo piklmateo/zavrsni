75450424157b4e62486c2f8fe98bc00f820c5b01
Mateo <piklmateo07@gmail.com>
2024-08-16 14:08:42 +0200

    Minor fixes.

--- app/src/components/ReservationComponents/ReservationsTableUser/ReservationsTableUser.tsx
+++ app/src/components/ReservationComponents/ReservationsTableUser/ReservationsTableUser.tsx
@@ -7,26 +7,19 @@ import {
 import { formatDate, formatTime } from "../../../helpers/dateTimeFormat";
 import "./ReservationsTableUser.css";
 import { AppDispatch, RootState } from "../../../state/store/store";
+import { Link } from "react-router-dom";
 
 const ReservationsTableUser = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const reservationsUserList = useSelector(
-    (state: RootState) => state.reservations.userReservations
-  );
-  const specialReservationsList = useSelector(
-    (state: RootState) => state.reservations.userSpecialReservations
-  );
+  const reservationsUserList = useSelector((state: RootState) => state.reservations.userReservations);
+  const specialReservationsList = useSelector((state: RootState) => state.reservations.userSpecialReservations);
   const status = useSelector((state: RootState) => state.reservations.status);
   const error = useSelector((state: RootState) => state.reservations.error);
 
   useEffect(() => {
-    if (status === "idle") {
-      setTimeout(() => {
-        dispatch(fetchUserReservations());
-        dispatch(fetchSpecialUserReservations());
-      }, 0);
-    }
-  }, [status, dispatch]);
+    dispatch(fetchUserReservations());
+    dispatch(fetchSpecialUserReservations());
+  }, [dispatch]);
 
   if (status === "loading") {
     return <div>Loading...</div>;
@@ -39,81 +32,104 @@ const ReservationsTableUser = () => {
   return (
     <>
       <div className="main__layout__container">
+        {reservationsUserList.length === 0 && specialReservationsList.length === 0 && (
+          <div className="empty-reservations-message">
+            <h1>No reservations avaliable</h1>
+            <h3>Book a table or the restaurant for your special occasion</h3>
+            <div className="reservation-page-links">
+              <Link className="reservation__link" to={"/reservation"}>
+                <button className="btn btn__login">Book a table</button>
+              </Link>
+              <Link className="reservation__link" to={"/special-occasions"}>
+                <button className="btn btn__login">Book the restaurant</button>
+              </Link>
+            </div>
+          </div>
+        )}
         <div className="reservations__table__wrapper">
-          <h1>Reservations</h1>
-          <table className="reservations__table">
-            <thead>
-              <tr>
-                <th>
-                  <h2>Date</h2>
-                </th>
-                <th>
-                  <h2>Time</h2>
-                </th>
-                <th>
-                  <h2>Table</h2>
-                </th>
-                <th>
-                  <h2>Name</h2>
-                </th>
-                <th>
-                  <h2>Email</h2>
-                </th>
-                <th>
-                  <h2>Phone</h2>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {reservationsUserList.map((reservation) => (
-                <tr key={reservation.id_reservation}>
-                  <td>{formatDate(reservation.date.toString())}</td>
-                  <td>{formatTime(reservation.time)}</td>
-                  <td>{reservation.table_number}</td>
-                  <td>{reservation.name}</td>
-                  <td>{reservation.email}</td>
-                  <td>{reservation.phone}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <h1>Special occasions</h1>
-          <table className="reservations__table">
-            <thead>
-              <tr>
-                <th>
-                  <h2>Date</h2>
-                </th>
-                <th>
-                  <h2>Time</h2>
-                </th>
-                <th>
-                  <h2>Table</h2>
-                </th>
-                <th>
-                  <h2>Name</h2>
-                </th>
-                <th>
-                  <h2>Email</h2>
-                </th>
-                <th>
-                  <h2>Phone</h2>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {specialReservationsList.map((reservation) => (
-                <tr key={reservation.id_reservation}>
-                  <td>{formatDate(reservation.date.toString())}</td>
-                  <td>{formatTime(reservation.time)}</td>
-                  <td>{reservation.table_number}</td>
-                  <td>{reservation.name}</td>
-                  <td>{reservation.email}</td>
-                  <td>{reservation.phone}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          {reservationsUserList.length !== 0 && (
+            <>
+              <h1>Reservations</h1>
+              <table className="reservations__table">
+                <thead>
+                  <tr>
+                    <th>
+                      <h2>Date</h2>
+                    </th>
+                    <th>
+                      <h2>Time</h2>
+                    </th>
+                    <th>
+                      <h2>Table</h2>
+                    </th>
+                    <th>
+                      <h2>Name</h2>
+                    </th>
+                    <th>
+                      <h2>Email</h2>
+                    </th>
+                    <th>
+                      <h2>Phone</h2>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {reservationsUserList.map((reservation) => (
+                    <tr key={reservation.id_reservation}>
+                      <td>{formatDate(reservation.date.toString())}</td>
+                      <td>{formatTime(reservation.time)}</td>
+                      <td>{reservation.table_number}</td>
+                      <td>{reservation.name}</td>
+                      <td>{reservation.email}</td>
+                      <td>{reservation.phone}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </>
+          )}
+
+          {specialReservationsList.length !== 0 && (
+            <>
+              <h1>Special occasions</h1>
+              <table className="reservations__table">
+                <thead>
+                  <tr>
+                    <th>
+                      <h2>Date</h2>
+                    </th>
+                    <th>
+                      <h2>Time</h2>
+                    </th>
+                    <th>
+                      <h2>Table</h2>
+                    </th>
+                    <th>
+                      <h2>Name</h2>
+                    </th>
+                    <th>
+                      <h2>Email</h2>
+                    </th>
+                    <th>
+                      <h2>Phone</h2>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {specialReservationsList.map((reservation) => (
+                    <tr key={reservation.id_reservation}>
+                      <td>{formatDate(reservation.date.toString())}</td>
+                      <td>{formatTime(reservation.time)}</td>
+                      <td>{reservation.table_number}</td>
+                      <td>{reservation.name}</td>
+                      <td>{reservation.email}</td>
+                      <td>{reservation.phone}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </>
+          )}
         </div>
       </div>
     </>
