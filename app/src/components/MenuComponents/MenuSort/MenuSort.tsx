c47c3a3722a2b56b254127fbbe3fa139488b29eb
Mateo <piklmateo07@gmail.com>
2024-08-21 19:58:44 +0200

    Made sort component generic

--- app/src/components/MenuComponents/MenuSort/MenuSort.tsx
+++ app/src/components/MenuComponents/MenuSort/MenuSort.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 import "./MenuSort.css";
 
-interface SortProps {
-  handleSortOrder: (category: string) => void;
+export interface SortOption {
+  value: string;
+  label: string;
 }
 
-const MenuSort = ({ handleSortOrder }: SortProps) => {
+export interface SortProps {
+  handleSortOrder: (category: string) => void;
+  options: SortOption[];
+  value: string;
+  name: string;
+}
+
+const MenuSort = ({ handleSortOrder, options, value, name }: SortProps) => {
   return (
     <div>
       <select
         className="menu__sort"
-        name="menu_sort"
-        id="menu_sort"
+        name={name}
+        id={name}
+        value={value}
         onChange={(event) => handleSortOrder(event.target.value)}
       >
-        <option value="price-asc">Price - Ascending</option>
-        <option value="price-desc">Price - Descending</option>
-        <option value="name-asc">Name - Ascending</option>
-        <option value="name-desc">Name - Descending</option>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
       </select>
     </div>
   );
