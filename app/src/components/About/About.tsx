e3d45fbd7f2fd073fb8750b2b9d965d6f7dc1cca
Mateo <piklmateo07@gmail.com>
2024-08-29 13:26:24 +0200

    Updated about text.

--- app/src/components/About/About.tsx
+++ app/src/components/About/About.tsx
@@ -10,14 +10,21 @@ const About = () => {
             <h2>About us</h2>
           </div>
           <div className="about__content">
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo dolore temporibus saepe impedit, sit
-              rem excepturi in recusandae, fugiat iste esse dolores accusamus voluptas, vero illo molestias reiciendis
-              at nemo. Voluptas veniam debitis iusto aperiam. Exercitationem numquam amet voluptates porro quam placeat
-              assumenda, et sunt suscipit nam ea sapiente, officiis necessitatibus libero corrupti consequatur adipisci
-              facilis, alias cupiditate deserunt iste? Veniam cupiditate odit explicabo eum, tempora quasi. Nemo iusto,
-              debitis repellendus quae possimus libero delectus quisquam animi, maxime eum suscipit eaque natus pariatur
-              quo doloribus excepturi modi. Sunt, dolorum voluptates.
+            <p className="text-justify">
+              Welcome to <span className="bold">Restaurant Vicino al cuore</span>, a place where culinary passion meets
+              authentic Italian traditions. Nestled in the heart of Avellino, our restaurant is a celebration of Italy’s
+              rich gastronomic heritage, offering a warm and inviting atmosphere that feels just like home. At{" "}
+              <span className="bold">Restaurant Vicino al cuore</span>, we believe that food is more than just
+              nourishment—it’s an experience. Our chefs craft each dish with care, using the finest ingredients to bring
+              the true flavors of Italy to your table. Our menu is designed to cater to all tastes. We take pride in our
+              commitment to quality, ensuring that every meal we serve is a testament to our love for good food and
+              great company.
+            </p>
+
+            <p className="text-justify">
+              Join us for a memorable dining experience, whether you're celebrating a special occasion or simply
+              enjoying a night out. At Vicino al cuore, you’re always close to the heart of exceptional dining. We look
+              forward to welcoming you and making your visit truly special.
             </p>
           </div>
         </div>
